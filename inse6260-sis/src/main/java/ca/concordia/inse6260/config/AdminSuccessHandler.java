cd881176fc3436db626bf216fa45e774aa1f35ca
Gallindo, Gustavo <gustavo.gallindo@sap.com>
2016-06-02 19:05:52 -0400

    success handler by role

---
+++ inse6260-sis/src/main/java/ca/concordia/inse6260/config/AdminSuccessHandler.java
@@ -0,0 +1,59 @@
+package ca.concordia.inse6260.config;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+public class AdminSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+	
+	
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+			throws ServletException, IOException {
+		// FIXME - check ROLE_PROFESSOR
+		boolean isAdmin = hasRole(authentication, "ROLE_ADMIN");
+		
+		if (isAdmin) {
+			handle(request, response, authentication);
+	        clearAuthenticationAttributes(request);
+		} else {
+			super.onAuthenticationSuccess(request, response, authentication);
+		}
+	}
+
+	@Override
+	protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		// FIXME - check ROLE_PROFESSOR
+		boolean isAdmin = hasRole(authentication, "ROLE_ADMIN");
+		
+		if (isAdmin) {
+			return "/";
+		} else {
+			return super.determineTargetUrl(request, response);
+		}
+	}
+	
+	private boolean hasRole(final Authentication authentication, final String role) {
+		boolean hasRole = false;
+		for (GrantedAuthority grantedAuthority : authentication.getAuthorities()) {
+			if (grantedAuthority.getAuthority().equals(role)) {
+				hasRole = true;
+				break;
+			}
+		}
+		return hasRole;
+	}
+	
+	
+
+	
+}
--- inse6260-sis/src/main/java/ca/concordia/inse6260/config/SecurityConfig.java
+++ inse6260-sis/src/main/java/ca/concordia/inse6260/config/SecurityConfig.java
@@ -3,11 +3,13 @@ package ca.concordia.inse6260.config;
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -20,8 +22,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and()//
-				.formLogin().loginPage("/login").permitAll().and()//
+				.formLogin().loginPage("/login").successHandler(adminSuccessHandler()).permitAll().and()//
 				.logout().permitAll();
+	}
+	
+	@Bean
+	public AuthenticationSuccessHandler adminSuccessHandler() {
+		return new AdminSuccessHandler();
 	}
 
 	@Resource
