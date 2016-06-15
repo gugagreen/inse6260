08bc6510d26918eac613c31a960b4b7b5033deaf
Max Lam <lamothe.max@gmail.com>
2016-06-15 01:24:31 -0400

    Small changes to scheduler, should now always load proper

--- inse6260-sis/src/main/resources/public/js/scheduler.js
+++ inse6260-sis/src/main/resources/public/js/scheduler.js
@@ -1,6 +1,11 @@
 const ROOT_PATH = "http://localhost:8080";
 
+
 $(document).ready(function() {
+	loadSchedule();
+});
+
+function makeCalendar(scheduledEvents){
 	
 	$('#calendar').fullCalendar({
 		header: {
@@ -8,9 +13,9 @@ $(document).ready(function() {
 			center: 'title',
 			right: 'month,agendaWeek,agendaDay'
 		},
-		editable: true,
-		eventLimit: true, // allow "more" link when too many events
-		events: loadSchedule(),//Put the event function here and remove the example below:
+		editable: false,
+		eventLimit: false, // allow "more" link when too many events
+		events: scheduledEvents,//Put the event function here and remove the example below:
 		eventRender: function(event){
 		    return (event.ranges.filter(function(range){ // test event against all the ranges
 
@@ -19,37 +24,41 @@ $(document).ready(function() {
 
 		    }).length)>0; //if it isn't in one of the ranges, don't render it (by returning false)
 		},	
-	});
-});
+	});	
+}
+
 
 function loadSchedule() {
 	var results = [];
 	if (isStudent()) {
 		var studentId = $("#username").val();
 		$('#currentStudent').val(studentId);
-		results = ajaxLoadScheduleForStudent(studentId);
+		ajaxLoadScheduleForStudent(studentId);
 	} 
-	return results;
 }
 
 function ajaxLoadScheduleForStudent(studentId) {
-	var results = [];
+	var scheduledEvents = [];
 
-	$.ajax({
-		url : ROOT_PATH + "/cart/student/" + studentId
-	}).then(function(data) {
-		for (var i = 0; i < data.length; i++) {
-			results.push({title: data[i].courseEntry.course.code,
-				start: data[i].courseEntry.dates.startTime,
-				end: data[i].courseEntry.dates.endTime,
-				dow: getDow(data[i]),
-				ranges:[
-				        {start:formatDate(data[i].courseEntry.dates.startDate), end:formatDate(data[i].courseEntry.dates.endDate)}
-				        ]
-			});
-	    }	
-	});
-	return results;
+	$.when($.ajax({
+		url : ROOT_PATH + "/cart/student/" + studentId,
+		success: function(data){
+			for (var i = 0; i < data.length; i++) {
+				scheduledEvents.push({title: data[i].courseEntry.course.code,
+					start: data[i].courseEntry.dates.startTime,
+					end: data[i].courseEntry.dates.endTime,
+					dow: getDow(data[i]),
+					ranges:[
+					        {start:formatDate(data[i].courseEntry.dates.startDate), end:formatDate(data[i].courseEntry.dates.endDate)}
+					        ]
+				});
+			}
+			return scheduledEvents;
+		}
+	})
+	).done(function(data){
+		makeCalendar(scheduledEvents);
+		});
 }
 
 function formatDate(date){
