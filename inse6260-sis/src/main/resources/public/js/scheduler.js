579102b0a64844aa9972d0299789b54575cb49a7
Max Lam <lamothe.max@gmail.com>
2016-06-15 00:14:31 -0400

    Finished functionality of scheduler, should be fully operational!

--- inse6260-sis/src/main/resources/public/js/scheduler.js
+++ inse6260-sis/src/main/resources/public/js/scheduler.js
@@ -1,3 +1,5 @@
+const ROOT_PATH = "http://localhost:8080";
+
 $(document).ready(function() {
 	
 	$('#calendar').fullCalendar({
@@ -8,22 +10,7 @@ $(document).ready(function() {
 		},
 		editable: true,
 		eventLimit: true, // allow "more" link when too many events
-
-		
-		
-
-		events: [//Put the event function here and remove the example below:
-			{ title:1, //This is the title of the event
-				start:"10:00", //This is the start time
-				end:"12:00",  //This is the end time
-				dow:[0,4], //This is the day of the week 0 being Sunday, 6 Being Saturday
-				ranges:[{start:"2016/03/01", end:"2016/07/01"},] //This is the start and end dates for the repeating event.
-			//remove the example above
-}
-		],
-		
-		
-		
+		events: loadSchedule(),//Put the event function here and remove the example below:
 		eventRender: function(event){
 		    return (event.ranges.filter(function(range){ // test event against all the ranges
 
@@ -31,9 +18,55 @@ $(document).ready(function() {
 		                event.end.isAfter(range.start));
 
 		    }).length)>0; //if it isn't in one of the ranges, don't render it (by returning false)
-		},
-		
-		
+		},	
 	});
-	
 });
+
+function loadSchedule() {
+	var results = [];
+	if (isStudent()) {
+		var studentId = $("#username").val();
+		$('#currentStudent').val(studentId);
+		results = ajaxLoadScheduleForStudent(studentId);
+	} 
+	return results;
+}
+
+function ajaxLoadScheduleForStudent(studentId) {
+	var results = [];
+
+	$.ajax({
+		url : ROOT_PATH + "/cart/student/" + studentId
+	}).then(function(data) {
+		for (var i = 0; i < data.length; i++) {
+			results.push({title: data[i].courseEntry.course.code,
+				start: data[i].courseEntry.dates.startTime,
+				end: data[i].courseEntry.dates.endTime,
+				dow: getDow(data[i]),
+				ranges:[
+				        {start:formatDate(data[i].courseEntry.dates.startDate), end:formatDate(data[i].courseEntry.dates.endDate)}
+				        ]
+			});
+	    }	
+	});
+	return results;
+}
+
+function formatDate(date){
+	var dateStamp = new Date(date);
+	var yyyy = dateStamp.getFullYear().toString();
+	var mm = (dateStamp.getMonth()+1).toString(); // getMonth() is zero-based
+    var dd  = dateStamp.getDate().toString();
+	return yyyy + "/" + (mm[1]?mm:"0"+mm[0]) + "/" + (dd[1]?dd:"0"+dd[0]);;
+}
+
+function getDow(data){
+	var array = [];
+	var weekdays = data.courseEntry.dates.weekDays;
+	for (var i =0; i< weekdays.length; i++){
+		if(weekdays.charAt(i)!== "-"){
+			array.push(i);
+		}
+	}
+	return array;
+}
