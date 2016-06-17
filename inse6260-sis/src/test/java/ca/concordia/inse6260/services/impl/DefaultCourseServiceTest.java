51906a970bf7d532a13a452162d99178bb07c485
Gallindo, Gustavo <gustavo.gallindo@sap.com>
2016-06-16 22:47:03 -0400

    fixing test after gpa changes

--- inse6260-sis/src/test/java/ca/concordia/inse6260/services/impl/DefaultCourseServiceTest.java
+++ inse6260-sis/src/test/java/ca/concordia/inse6260/services/impl/DefaultCourseServiceTest.java
@@ -1,5 +1,9 @@
 package ca.concordia.inse6260.services.impl;
 
+import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -14,8 +18,15 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import ca.concordia.inse6260.dao.CourseEntryDAO;
 import ca.concordia.inse6260.dao.StudentDAO;
+import ca.concordia.inse6260.entities.AcademicRecordEntry;
+import ca.concordia.inse6260.entities.Course;
+import ca.concordia.inse6260.entities.CourseDates;
 //import ca.concordia.inse6260.entities.AcademicRecordEntry;
 import ca.concordia.inse6260.entities.CourseEntry;
+import ca.concordia.inse6260.entities.Student;
+import ca.concordia.inse6260.entities.dto.Transcript;
+import ca.concordia.inse6260.entities.enums.AcademicRecordStatus;
+import ca.concordia.inse6260.entities.enums.Grade;
 //import ca.concordia.inse6260.entities.Student;
 //import ca.concordia.inse6260.entities.dto.Transcript;
 //import ca.concordia.inse6260.entities.enums.AcademicRecordStatus;
@@ -67,30 +78,34 @@ public class DefaultCourseServiceTest {
 
 		service.findBySeason(yearSeason);
 	}
-	//test currently removed due to changes in implementation
-	/*@Test
+
+	@Test
 	public void shouldHaveBGpa() {
 		String studentId = "testStudent";
 		Grade[] grades = new Grade[] { Grade.A, Grade.B_MINUS, Grade.NOT_SET, Grade.C_PLUS };
+		CourseEntry entry = buildCourseEntry(4, Season.SUMMER, string2Calendar("16.05.2016"));
+		CourseEntry[] courseEntries = new CourseEntry[] { entry, entry, entry, entry };
 		AcademicRecordStatus[] status = new AcademicRecordStatus[] { AcademicRecordStatus.FINISHED, AcademicRecordStatus.FINISHED,
 				AcademicRecordStatus.WAIT_LIST, AcademicRecordStatus.FINISHED };
-		Student student = buildStudentWithRecords(studentId, grades, status);
+		Student student = buildStudentWithRecords(studentId, grades, courseEntries, status);
 		Mockito.when(studentDao.findOne(studentId)).thenReturn(student);
 
 		Transcript transcript = service.getStudentTranscript(studentId);
 		Assert.assertNotNull(transcript);
-		Assert.assertEquals("3.34", transcript.getGpa());
+		// GPA should be => A(4.0) + B-(2.7) + C+(2.3) = 9.0/3 = 3.0
+		Assert.assertEquals(new BigDecimal("3.00").stripTrailingZeros(),
+				new BigDecimal(transcript.getGpa()).stripTrailingZeros());
 	}
-	
-	
-	//test currently removed due to changes in implementation
+
 	@Test
 	public void shouldOnlyHaveFinishedCoursesInTranscript() {
 		String studentId = "testStudent";
 		Grade[] grades = new Grade[] { Grade.A, Grade.B_MINUS, Grade.NOT_SET, Grade.C_PLUS };
+		CourseEntry entry = buildCourseEntry(4, Season.SUMMER, string2Calendar("16.05.2016"));
+		CourseEntry[] courseEntries = new CourseEntry[] { entry, entry, entry, entry };
 		AcademicRecordStatus[] status = new AcademicRecordStatus[] { AcademicRecordStatus.FINISHED, AcademicRecordStatus.FINISHED,
 				AcademicRecordStatus.WAIT_LIST, AcademicRecordStatus.FINISHED };
-		Student student = buildStudentWithRecords(studentId, grades, status);
+		Student student = buildStudentWithRecords(studentId, grades, courseEntries, status);
 		Mockito.when(studentDao.findOne(studentId)).thenReturn(student);
 
 		Transcript transcript = service.getStudentTranscript(studentId);
@@ -98,7 +113,8 @@ public class DefaultCourseServiceTest {
 		Assert.assertEquals(3, transcript.getAcademicRecords().size());
 	}
 
-	private Student buildStudentWithRecords(final String studentId, final Grade[] grades, final AcademicRecordStatus[] status) {
+	private Student buildStudentWithRecords(final String studentId, final Grade[] grades, CourseEntry[] courseEntries,
+			final AcademicRecordStatus[] status) {
 		Student student = new Student();
 		student.setUsername(studentId);
 		List<AcademicRecordEntry> academicRecords = new ArrayList<>();
@@ -106,9 +122,35 @@ public class DefaultCourseServiceTest {
 			AcademicRecordEntry record = new AcademicRecordEntry();
 			record.setGrade(grades[i]);
 			record.setStatus(status[i]);
+			record.setCourseEntry(courseEntries[i]);
 			academicRecords.add(record);
 		}
 		student.setAcademicRecords(academicRecords);
 		return student;
-	}*/
+	}
+
+	private CourseEntry buildCourseEntry(int credits, final Season season, final Calendar startDate) {
+		Course course = new Course();
+		course.setCredits(credits);
+
+		CourseDates dates = new CourseDates();
+		dates.setSeason(season);
+		dates.setStartDate(startDate);
+
+		CourseEntry courseEntry = new CourseEntry();
+		courseEntry.setCourse(course);
+		courseEntry.setDates(dates);
+		return courseEntry;
+	}
+
+	private Calendar string2Calendar(final String dateString) {
+		DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
+		Calendar cal = Calendar.getInstance();
+		try {
+			cal.setTime(df.parse(dateString));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return cal;
+	}
 }
